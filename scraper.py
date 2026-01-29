b22bb9fd7ee7319d1324408b1b629c6225b6daf9
Gary Cheng <d9369604@gmail.com>
2026-01-29 20:50:15 +0800

    Rename variable

--- scraper.py
+++ scraper.py
@@ -1,5 +1,4 @@
 import re
-from unittest import result
 
 import httpx, json
 from bs4 import BeautifulSoup, Tag
@@ -46,11 +45,11 @@ def get_ingredients(ingredients_content_tag: Tag) -> dict:
     }
 
 def get_recipe_info(soup: BeautifulSoup) -> dict:
-    result = {}
+    recipe_info = {}
 
-    result["name"] = soup.select_one('h1[class="l-single-header__title"]').text.strip()
-    result["category"] = soup.select_one('span[class="l-single-header__tag"]').text.strip()
-    result["description"] = soup.select_one('section[class="l-single-content"]').text.strip()
+    recipe_info["name"] = soup.select_one('h1[class="l-single-header__title"]').text.strip()
+    recipe_info["category"] = soup.select_one('span[class="l-single-header__tag"]').text.strip()
+    recipe_info["description"] = soup.select_one('section[class="l-single-content"]').text.strip()
 
     ingredients_content = soup.select_one('section[class="l-single-meet"]')
 
@@ -60,28 +59,28 @@ def get_recipe_info(soup: BeautifulSoup) -> dict:
         ingredients_content.select_one("h2 span").get_text(strip=True)
     )
 
-    result["quantity"] = quantity
+    recipe_info["quantity"] = quantity
 
     ingredient = get_ingredients(ingredients_content)
     seasoning = get_seasoning(ingredients_content)
 
     if seasoning:
-        result["groups"] = [ingredient, seasoning]
+        recipe_info["groups"] = [ingredient, seasoning]
     else:
-        result["groups"] = [ingredient]
-    result["steps"] = get_steps(soup)
+        recipe_info["groups"] = [ingredient]
+    recipe_info["steps"] = get_steps(soup)
 
-    return result
+    return recipe_info
 
 def scrape_recipe(url):
     page = httpx.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
 
-    result = get_recipe_info(soup)
+    recipe = get_recipe_info(soup)
 
-    print(json.dumps(result, ensure_ascii=False, indent=2))
+    print(json.dumps(recipe, ensure_ascii=False, indent=2))
 
-
+    return recipe
 
 
 if __name__ == "__main__":
