ae2723fc6d6fa6b958275a78938760d7f90aef8c
Gary Cheng <d9369604@gmail.com>
2026-02-17 06:22:29 +0800

    Add Semaphore to producer

--- web_crawler/service/tasty_note_service.py
+++ web_crawler/service/tasty_note_service.py
@@ -9,6 +9,7 @@ import asyncio
 
 LIST_URL = "https://tasty-note.com/tag/ten-minutes/page/{}/"
 MAX_PAGE_SIZE = 2
+MAX_WORKER = 5
 
 class TastyNoteService:
     def __init__(self, list_crawler: TastyNoteListCrawler, detail_crawler: TastyNoteDetailCrawler, requester: HttpxRequester):
@@ -16,25 +17,15 @@ class TastyNoteService:
         self._detail_crawler = detail_crawler
         self._requester = requester
 
-    async def _get_detail_urls(self, list_url, url_queue: asyncio.Queue):
-        html = await self._requester.request(list_url)
-        for detail in await self._list_crawler.crawl(html):
-            await url_queue.put(detail.get_url())
-            print("Added {} to queue".format(detail.get_url()))
-
-    async def _get_recipe(self, url: str) -> TastyNoteDetail:
-        html = await self._requester.request(url)
-        return self._detail_crawler.crawl(html)
-
     async def fetch_recipes(self) -> AsyncGenerator:
-        url_queue = asyncio.Queue(maxsize=20)
+        url_queue = asyncio.Queue(maxsize=30)
         result_queue = asyncio.Queue()
 
         # 啟動生產者與消費者
-        producer_task = asyncio.create_task(self.producer(url_queue))
+        producer_task = asyncio.create_task(self._producer(url_queue))
         consumer_task = [
-            asyncio.create_task(self.consumer(url_queue, result_queue))
-            for _ in range(5)
+            asyncio.create_task(self._consumer(url_queue, result_queue))
+            for _ in range(MAX_WORKER)
         ]
 
         # 等待生產者做完
@@ -51,18 +42,32 @@ class TastyNoteService:
         while not result_queue.empty():
             yield await result_queue.get()
 
-    async def producer(self, url_queue: asyncio.Queue):
+    async def _producer(self, url_queue: asyncio.Queue):
+        sem = asyncio.Semaphore(3)  # 列表頁抓取可以更嚴格一點，限制同時 3 頁
+
+        async def get_detail_urls(list_url, url_queue: asyncio.Queue):
+            async with sem:
+                html = await self._requester.request(list_url)
+                for detail in await self._list_crawler.crawl(html):
+                    await url_queue.put(detail.get_url())
+                    print("Added {} to queue".format(detail.get_url()))
+
         tasks = [
-            self._get_detail_urls(LIST_URL.format(page), url_queue)
+            get_detail_urls(LIST_URL.format(page), url_queue)
             for page in range(2, MAX_PAGE_SIZE + 1)
         ]
 
         await asyncio.gather(*tasks)
 
-    async def consumer(self, url_queue: asyncio.Queue, result_queue: asyncio.Queue):
+    async def _consumer(self, url_queue: asyncio.Queue, result_queue: asyncio.Queue):
+
+        async def get_recipe(url: str) -> TastyNoteDetail:
+            html = await self._requester.request(url)
+            return self._detail_crawler.crawl(html)
+
         while True:
             url = await url_queue.get()
-            recipe = await self._get_recipe(url)
+            recipe = await get_recipe(url)
             await result_queue.put(recipe)
             print("Added {}".format(recipe.name))
             url_queue.task_done()
