2526f11d49b2c89d2bcfc6a379e403ab51f22b8b
Gary Cheng <d9369604@gmail.com>
2026-02-18 00:27:26 +0800

    Add storage worker

--- web_crawler/main.py
+++ web_crawler/main.py
@@ -1,24 +1,34 @@
 from web_crawler.service import get_tasty_note_crawler_service
+from app.services.ingestion import get_ingestion_service, IngestionService
 import asyncio
 
+async def storage_worker(queue: asyncio.Queue, service: IngestionService):
+    """這是一個獨立的工人，專門搬運資料庫"""
+    while True:
+        recipe = await queue.get()
+        try:
+            await service.ingest_recipe(recipe)
+        finally:
+            queue.task_done()
 
 async def main():
     crawler = get_tasty_note_crawler_service()
 
-    async for recipe in crawler.fetch_recipes():
-        print(recipe)
+    async with get_ingestion_service() as ingestion_service:
+        producer_task, consumer_task, url_queue, result_queue = await crawler.fetch_recipes()
 
-    # async with get_ingestion_service() as ingestion_service:
-    #     ingestion_service.ingest_recipe()
+        storage_task = asyncio.create_task(storage_worker(result_queue, ingestion_service))
+
+        try:
+            await producer_task
+            await url_queue.join()
+            await result_queue.join()
+
+        finally:
+            for c in consumer_task:
+                c.cancel()
+            storage_task.cancel()
 
 if __name__ == "__main__":
     asyncio.run(main())
-    # url = "https://tasty-note.com/tag/ten-minutes/page/1/"
-    # crawler = get_tasty_note_crawler_service()
-    # crawler._get_detail_urls(url)
-    # for url in crawler._get_detail_urls(url):
-    #     print(url)
-    # print(crawler.get_recipe_test("https://tasty-note.com/yam-with-raw-egg/"))
-
-
 
--- web_crawler/service/tasty_note_service.py
+++ web_crawler/service/tasty_note_service.py
@@ -1,10 +1,8 @@
-from typing import AsyncGenerator
-
 from web_crawler.list_crawler import TastyNoteListCrawler
 from web_crawler.detail_crawler import TastyNoteDetailCrawler
 from web_crawler.requester import HttpxRequester
 from web_crawler.schema import TastyNoteRecipe
-import asyncio
+import asyncio, random
 
 
 LIST_URL = "https://tasty-note.com/tag/ten-minutes/page/{}/"
@@ -17,9 +15,9 @@ class TastyNoteService:
         self._detail_crawler = detail_crawler
         self._requester = requester
 
-    async def fetch_recipes(self) -> AsyncGenerator:
-        url_queue = asyncio.Queue(maxsize=30)
-        result_queue = asyncio.Queue()
+    async def fetch_recipes(self):
+        url_queue = asyncio.Queue(maxsize=20)
+        result_queue = asyncio.Queue(maxsize=20)
 
         # 啟動生產者與消費者
         producer_task = asyncio.create_task(self._producer(url_queue))
@@ -28,25 +26,28 @@ class TastyNoteService:
             for _ in range(MAX_WORKER)
         ]
 
-        # 等待生產者做完
-        await asyncio.gather(producer_task)
+        return producer_task, consumer_task, url_queue, result_queue
 
-        # 等待隊列中剩下的任務被處理完
-        await url_queue.join()
-
-        # 關閉消費者 (因為它原本是死迴圈)
-        for c in consumer_task:
-            c.cancel()
-
-        # 最後把 result_queue 裡的東西 yield 出去
-        while not result_queue.empty():
-            yield await result_queue.get()
+        # # 等待生產者做完
+        # await asyncio.gather(producer_task)
+        #
+        # # 等待隊列中剩下的任務被處理完
+        # await url_queue.join()
+        #
+        # # 關閉消費者 (因為它原本是死迴圈)
+        # for c in consumer_task:
+        #     c.cancel()
+        #
+        # # 最後把 result_queue 裡的東西 yield 出去
+        # while not result_queue.empty():
+        #     yield await result_queue.get()
 
     async def _producer(self, url_queue: asyncio.Queue):
         sem = asyncio.Semaphore(3)  # 列表頁抓取可以更嚴格一點，限制同時 3 頁
 
         async def get_detail_urls(list_url, url_queue: asyncio.Queue):
             async with sem:
+                await asyncio.sleep(random.uniform(0.5, 1.5))
                 html = await self._requester.request(list_url)
                 for detail in self._list_crawler.crawl(html):
                     await url_queue.put(detail.get_url())
@@ -67,10 +68,14 @@ class TastyNoteService:
 
         while True:
             url = await url_queue.get()
-            recipe = await get_recipe(url)
-            await result_queue.put(recipe)
-            print("Added {}".format(recipe.name))
-            url_queue.task_done()
+            try:
+                recipe = await get_recipe(url)
+                await result_queue.put(recipe)
+                print("Added {}".format(recipe.name))
+            finally:
+                # 這是關鍵！不論成功失敗，都要告訴 queue「這件事我做完了」
+                # 這樣最外層的 await url_queue.join() 才會通過
+                url_queue.task_done()
 
 
 def get_tasty_note_crawler_service():
