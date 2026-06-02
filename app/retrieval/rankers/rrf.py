744636baa89d0eba2e64b7e28fff32fe8deacde0
Gary Cheng <d9369604@gmail.com>
2026-06-02 13:57:55 +0800

    Add RRF ranker

---
+++ app/retrieval/rankers/rrf.py
@@ -0,0 +1,34 @@
+from app.schema import RRFResult
+
+
+class RRFRanker:
+
+    @staticmethod
+    def reciprocal_rank_fusion(search_results_list, k=60) -> list[RRFResult]:
+        """
+        search_results_list: 一個列表的列表，例如 [[doc_id1, doc_id2], [doc_id2, doc_id3]]
+        k: 平滑常數，預設 60
+        """
+        if not search_results_list:
+            return []
+
+        fused_scores = {}
+
+        for rank_list in search_results_list:
+            for rank, doc_id in enumerate(rank_list):
+                # rank 從 0 開始，所以公式中要 +1
+                # 如果找不到 doc_id，就回傳 0，然後再加分數
+                fused_scores[doc_id] = fused_scores.get(doc_id, 0) + 1 / (k + (rank + 1))
+
+        # 按分數從高到低排序
+        """
+        fused_scores 是一個字典（Dictionary），長得像這樣： {"doc_A": 0.032, "doc_B": 0.015, "doc_C": 0.045}
+        當你執行 .items() 時，它會變成一個列表包著元組（list of tuples）： [("doc_A", 0.032), ("doc_B", 0.015), ("doc_C", 0.045)]
+        用分數做排序，分數愈高越前面
+        """
+        sorted_results = sorted(fused_scores.items(), key=lambda x: x[1], reverse=True)
+
+        return [
+            RRFResult(id=idx, score=score)
+            for idx, score in sorted_results
+        ]
---
+++ tests/unittest/test_rrf.py
@@ -0,0 +1,46 @@
+from app.retrieval.rankers.rrf import RRFRanker
+
+
+def test_rrf_two_rank_list_then_a_got_top_rank():
+    list_1 = ["A", "B", "C"]
+    list_2 = ["D", "A", "B"]
+
+    rrf_ranker = RRFRanker()
+
+    fused = rrf_ranker.reciprocal_rank_fusion([list_1, list_2])
+
+    assert fused[0].id == "A"
+
+
+def test_rrf_two_rank_list_then_a_and_b_have_same_rank():
+    list_1 = ["A", "B", "C"]
+    list_2 = ["B", "A", "D"]
+
+    rrf_ranker = RRFRanker()
+
+    fused = rrf_ranker.reciprocal_rank_fusion([list_1, list_2])
+
+    scores = {r.id: r.score for r in fused}
+
+    assert scores["A"] == scores["B"]
+
+
+def test_rrf_two_rank_list_then_all_elements_exist():
+    list_1 = ["A", "B", "C"]
+    list_2 = ["B", "A", "D"]
+
+    rrf_ranker = RRFRanker()
+
+    fused = rrf_ranker.reciprocal_rank_fusion([list_1, list_2])
+
+    ids = {r.id for r in fused}
+
+    assert ids == {"A", "B", "C", "D"}
+
+
+def test_rrf_empty_list_then_return_empty_list():
+    rrf_ranker = RRFRanker()
+
+    fused = rrf_ranker.reciprocal_rank_fusion([])
+
+    assert fused == []
