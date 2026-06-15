89a4c1cd5601a19034decfa5ef12107e2b8a09eb
Gary Cheng <d9369604@gmail.com>
2026-06-16 00:25:14 +0800

    Add test for save raw data stage

--- youtube/stage/save_raw_data.py
+++ youtube/stage/save_raw_data.py
@@ -1,6 +1,8 @@
 from app.database import AsyncSessionLocal
 from app.repositories.yt_repository import YtRepository
+from youtube.domain.mapper.section import SectionMapper
 from youtube.domain.mapper.source import SourceMapper
+from youtube.domain.models import Source, Section
 from youtube.domain.video_document import VideoDocument
 
 
@@ -10,12 +12,12 @@ class SaveRawData:
         self.session_factory = session_factory
 
     async def run(self, document: VideoDocument) -> VideoDocument:
-        source_model = SourceMapper.from_document(document)
-        sections_model = SourceMapper.from_document(document)
+        source_model: Source = SourceMapper.from_document(document)
+        section_models: list[Section] = SectionMapper.from_document(document)
 
         async with self.session_factory() as session:
             async with session.begin():
                 await self.repository.insert(session, source_model)
-                await self.repository.insert(session, sections_model)
+                await self.repository.insert_bulk(session, section_models)
 
         return document
---
+++ youtube/tests/test_save_raw_data.py
@@ -0,0 +1,39 @@
+from unittest.mock import AsyncMock, MagicMock, Mock, patch
+
+import pytest
+
+from youtube.domain.mapper.section import SectionMapper
+from youtube.domain.mapper.source import SourceMapper
+from youtube.stage.save_raw_data import SaveRawData
+
+
+@pytest.fixture
+def mock_session_factory():
+    mock_session = AsyncMock()
+    mock_session.__aenter__ = AsyncMock(return_value=mock_session)
+    mock_session.__aexit__ = AsyncMock(return_value=None)
+
+    mock_session.begin = MagicMock(return_value=mock_session)
+
+    factory = MagicMock(return_value=mock_session)
+    return factory
+
+
+@pytest.mark.asyncio
+async def test_save_raw_data(mock_session_factory):
+    mock_repository = Mock()
+    mock_repository.insert = AsyncMock()
+    mock_repository.insert_bulk = AsyncMock()
+
+    with patch.object(SourceMapper, 'from_document', MagicMock(return_value="insert Source")) as mock_source:
+        with patch.object(SectionMapper, 'from_document', MagicMock(return_value="insert Sections")) as mock_sections:
+            mock_session = mock_session_factory.return_value
+
+            stage = SaveRawData(mock_repository, mock_session_factory)
+
+            await stage.run(MagicMock())
+
+            assert mock_source.call_count == 1
+            assert mock_sections.call_count == 1
+            mock_repository.insert.assert_called_once_with(mock_session, "insert Source")
+            mock_repository.insert_bulk.assert_called_once_with(mock_session, "insert Sections")
