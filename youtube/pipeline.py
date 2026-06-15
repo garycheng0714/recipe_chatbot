9e8d504bfebd8d2837de25095542cf153abf011e
Gary Cheng <d9369604@gmail.com>
2026-06-16 03:36:18 +0800

    Add stages to run pipeline

--- youtube/pipeline.py
+++ youtube/pipeline.py
@@ -1,12 +1,38 @@
+import asyncio
+
+from app.repositories.yt_repository import YtRepository
+from web_crawler.requester import HttpxRequester
 from youtube.domain.video_document import VideoDocument
 from youtube.stage.base import Stage
+from youtube.stage.fetch_video import FetchVideoTransformer
+from youtube.stage.save_raw_data import SaveRawData
+from youtube.stage.transcript_segmenter import TranscriptSegmenter
+from youtube.video import YouTubeVideo
 
 
 class Pipeline:
     def __init__(self, stages: list[Stage]):
         self.stages = stages
 
-    def run(self, context: VideoDocument):
+    async def run(self, context: VideoDocument):
         for stage in self.stages:
-            context = stage.run(context)
+            context = await stage.run(context)
         return context
+
+
+async def main():
+    async with HttpxRequester() as requester:
+        pipeline = Pipeline([
+            FetchVideoTransformer(yt=YouTubeVideo(requester)),
+            TranscriptSegmenter(),
+            SaveRawData(repository=YtRepository())
+        ])
+
+        context = VideoDocument(id="7E6TNeoOC3Y")
+
+        result = await pipeline.run(context)
+        print(result)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
