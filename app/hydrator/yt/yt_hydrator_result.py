35c9b2f19972487a44e5c1724cba74a70b60af9e
Gary Cheng <d9369604@gmail.com>
2026-07-21 14:09:58 +0800

    Add id to hydrator result

--- app/hydrator/yt/yt_hydrator_result.py
+++ app/hydrator/yt/yt_hydrator_result.py
@@ -1,9 +1,12 @@
+from uuid import UUID
+
 from pydantic import BaseModel, ConfigDict
 
 
 class YtHydratorResult(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
+    id: UUID
     question: str
     answer: str
     topic: str
