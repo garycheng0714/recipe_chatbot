1b76ff8ce48017c5bbc3f01fafcd7f2fc94dbcfb
Gary Cheng <d9369604@gmail.com>
2026-08-08 23:05:11 +0800

    Modify function name

--- app/main.py
+++ app/main.py
@@ -70,7 +70,7 @@ async def search_recipe(
     query_text: str,
     service: RetrievalService = Depends(get_recipe_retrieval_service)
 ):
-    obj_list = await service.search_recipe(query_text)
+    obj_list = await service.search(query_text)
 
     # 安全檢查：找不到就報 404，不要讓後續程式碼崩潰
     if obj_list is None:
@@ -79,25 +79,11 @@ async def search_recipe(
     return obj_list
 
 @app.get("/yt/search/{query_text}")
-async def search_recipe(
+async def search_yt(
     query_text: str,
     service: RetrievalService = Depends(get_yt_retrieval_service)
 ):
-    obj_list = await service.search_recipe(query_text)
-
-    # 安全檢查：找不到就報 404，不要讓後續程式碼崩潰
-    if obj_list is None:
-        raise HTTPException(status_code=404, detail="Recipe not found")
-
-    return obj_list
-
-
-@app.get("/yt/search-answer/{query_text}")
-async def search_recipe(
-    query_text: str,
-    service: RetrievalService = Depends(get_yt_answer_retrieval_service)
-):
-    obj_list = await service.search_recipe(query_text)
+    obj_list = await service.search(query_text, 10)
 
     # 安全檢查：找不到就報 404，不要讓後續程式碼崩潰
     if obj_list is None:
@@ -112,10 +98,6 @@ async def es_search(query: str, retriever: RetrieverBase = Depends(get_yt_es_ret
 
 @app.get("/yt/qdr/{query}")
 async def qdr_search(query: str, retriever: RetrieverBase = Depends(get_yt_qdr_retriever)):
-    return await retriever.retrieve(query, 5)
-
-@app.get("/yt/qdr-answer/{query}")
-async def qdr_search(query: str, retriever: RetrieverBase = Depends(get_yt_answer_qdr_retriever)):
     return await retriever.retrieve(query, 5)
 
 @app.get("/es/{query}")
--- app/services/retriever.py
+++ app/services/retriever.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from app.hydrator.base_hydrator import BaseHydrator
 from app.retriever.hybrid_retriever import HybridRetriever
 
@@ -10,8 +12,8 @@ class RetrievalService:
         self.hybrid_retriever = hybrid_retriever
         self.hydrator = hydrator
 
-    async def search_recipe(self, query_text):
-        hybrid_results = await self.hybrid_retriever.retrieve(query_text, top_k=5)
+    async def search(self, query_text: str, top_k: int) -> list[dict[str, Any]]:
+        hybrid_results = await self.hybrid_retriever.retrieve(query_text, top_k=top_k)
 
         ids = [r.id for r in hybrid_results]
 
